6d65bf263d049a54cf5542a4bf31db579026c61c
Nitin Kumar <nitin.kumar@smu.ca>
2022-03-02 17:09:23 -0400

    Updated connection string and logger

--- src/main/java/com/hotel/demoHotel/controller/HotelController.java
+++ src/main/java/com/hotel/demoHotel/controller/HotelController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import com.hotel.demoHotel.model.Hotel;
 import com.hotel.demoHotel.model.MessageResponse;
 import com.hotel.demoHotel.service.HotelService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,33 +27,38 @@ public class HotelController {
 	
 	  @Autowired
       HotelService hotelService;
+    final static Logger logger = Logger.getLogger(HotelController.class);
 	  
 	@GetMapping("/all")
     public ResponseEntity<List<Hotel>> getAllHotelDetails () {
-        System.out.println("ye chal raja jao");
+        logger.info("GET: All Hotel list");
         List<Hotel> hotelList = hotelService.getAllHotel();
         return new ResponseEntity<>(hotelList, HttpStatus.OK);
     }
 	
 	@GetMapping("/find/{id}")
     public ResponseEntity<Hotel> getHotelDetailsById (@PathVariable("id") Integer id) {
+        logger.info("GET: getting the hotel details with id :"+id);
         Hotel hotel = hotelService.getHotelDetailsById(id);
         return new ResponseEntity<>(hotel, HttpStatus.OK);
     }
     @PostMapping("/add")
     public ResponseEntity<MessageResponse> addHotel(@RequestBody Hotel hotel) {
-        MessageResponse newEmployee = hotelService.createHotel(hotel);
-        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+        logger.info("POST: adding new hotel to database with name:"+hotel.getHotelname());
+        MessageResponse hotelCreated = hotelService.createHotel(hotel);
+        return new ResponseEntity<>(hotelCreated, HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
     public Optional<Hotel> updateHotel(@PathVariable Integer id, @RequestBody Hotel hotel) {
+        logger.info("PUT: update hotel details for hotel :"+hotel.getHotelname());
         return hotelService.updateHotel(id, hotel);
 
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteHotel(@PathVariable("id") Integer id) {
+        logger.info("Delete : delete hotel details for  hotelid:"+id);
     	hotelService.deleteHotel(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
